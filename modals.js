ea28abc7a91c2a21e2095fd168a273512a58a0b2
Jarosław Bagnicki <jarek@bagnicki.net>
2018-08-13 05:55:51 +0200

    Remove unessesery comments

--- modals.js
+++ modals.js
@@ -1,20 +1,13 @@
 'use strict';
 (function(){ 
-	/* W kodzie HTML i CSS dodaliśmy style dla prostego modala, który będzie zawsze wyśrodkowany w oknie. 
-	
-	Teraz wystarczy napisać funkcję otwierającą modal:
-	*/
-	
+
 	var showModal = function(event){
 		event.preventDefault();
     document.querySelector('#modal-overlay').classList.add('show');
     const modalId = event.target.getAttribute('href');
     const modalEl = document.querySelector(modalId);
-    // modalEl.querySelector('header').innerHTML = 'Modal header';
     modalEl.classList.add('show');
 	};
-	
-	// Mimo, że obecnie mamy tylko jeden link, stosujemy kod dla wielu linków. W ten sposób nie będzie trzeba go zmieniać, kiedy zechcemy mieć więcej linków lub guzików otwierających modale
 	
 	var modalLinks = document.querySelectorAll('.show-modal');
 	
@@ -22,8 +15,6 @@
 		modalLinks[i].addEventListener('click', showModal);
 	}
 	
-	// Dodajemy też funkcję zamykającą modal, oraz przywiązujemy ją do kliknięć na elemencie z klasą "close". 
-
 	var hideModal = function(event){
     event.preventDefault();
     document.querySelectorAll('.modal').forEach(modal => modal.classList.remove('show'));
@@ -36,11 +27,7 @@
 		closeButtons[i].addEventListener('click', hideModal);
 	}
 	
-	// Dobrą praktyką jest również umożliwianie zamykania modala poprzez kliknięcie w overlay. 
-	
 	document.querySelector('#modal-overlay').addEventListener('click', hideModal);
-	
-	// Musimy jednak pamiętać, aby zablokować propagację kliknięć z samego modala - inaczej każde kliknięcie wewnątrz modala również zamykałoby go. 
 	
 	var modals = document.querySelectorAll('.modal');
 	
@@ -49,11 +36,5 @@
 			event.stopPropagation();
 		});
 	}
-	
-	/* I to wszystko - mamy już działający modal! 
-	
-	ĆWICZENIE: 
-	Zmień funkcję showModal tak, aby w momencie wyświetlania była zmieniana treść nagłówka na dowolną inną, np. "Modal header". 
-	*/
 	
 })(); 
